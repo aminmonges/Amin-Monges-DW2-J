70be5a0d353a5a4b13247a38daac6ae01cdc8076
aminmonges <amin.monges123@gmail.com>
2024-04-25 19:30:03 -0300

    desafio 3

---
+++ script.js
@@ -0,0 +1,13 @@
+// DESAFIO 3
+let nombreProducto = "Zapatillas 2.2"
+let precioUnitario = 100000
+let cantidadDeseada = prompt("ingrese la cantidad deseada")
+
+if(cantidadDeseada >= 5){
+    let descuentol = (cantidadDeseada * precioUnitario) * 0.10
+    let precioSubfinal = cantidadDeseada + precioUnitario
+    let precioFinal = precioSubfinal - descuento
+
+
+}else{
+    
